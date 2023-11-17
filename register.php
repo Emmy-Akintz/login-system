08bf12a84ef970bae3151b84380aa3383f544021
Ayomide Akintan <emzyakints2005@gmail.com>
2023-11-17 13:28:29 +0100

    Update

--- register.php
+++ register.php
@@ -1,32 +1,44 @@
 <?php if (isset($_POST['register'])) {
-    // connect to the database
-    $mysqli = new mysqli("localhost", "username", "password", "login-system");
+            // connect to the database
+            $mysqli = new mysqli("localhost", "username", "password", "login_system");
 
-    // check for errors
-    if ($mysqli->connect_error)  { die("Connection failed: " . $mysqli->connect_error); }
+            // check for errors
+            if ($mysqli->connect_error)  {
+                die("Connection failed: " . $mysqli->connect_error); 
+            }
 
-    // prepare and bind the SQL statement
-    $stmt = $mysqli->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)"); $stmt->bind_param("sss", $username, $email, $password);
+            // prepare and bind the SQL statement
+            $stmt = $mysqli->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)"); $stmt->bind_param("sss", $username, $email, $password);
 
-    // Get the form data
-    $username = $_POST['username']; $email = $_POST['email']; $password = $_POST['password'];
+            // Get the form data
+            $username = $_POST['username']; $email = $_POST['email']; $password = $_POST['password'];
 
-    // Hash the password
-    $password = password_hash($password, PASSWORD_DEFAULT);
+            // Hash the password
+            $password = password_hash($password, PASSWORD_DEFAULT);
 
-    // Execute the SQL statement
-    if ($stmt->execute()) { echo "New account created successfully!"; } else { echo "Error: " . $stmt->error; }
+            // Execute the SQL statement
+            if ($stmt->execute()) { echo "New account created successfully!"; } else { echo "Error: " . $stmt->error; }
 
-    // Close the connection
-    $stmt->close(); $mysqli->close(); }
-?>
+            // Close the connection
+            $stmt->close(); $mysqli->close(); }
+        ?>
 
-<form action="register.php" method="post">
-    <label for="username">Username: </label>
-    <input id="username" name="username" required="" type="text" />
-    <label for="email"> Email: </label>
-    <input for="email" name="email" required="" type="email" />
-    <label for="password">Password: </label>
-    <input id="password" name="password" required="" type="password" />
-    <input name="register" type="submit" value="Register" />
-</form>
+<!DOCTYPE html>
+
+<html>
+    <body>
+
+        
+
+        <form action="register.php" method="post">
+            <label for="username">Username: </label>
+            <input id="username" name="username" required="" type="text" />
+            <label for="email"> Email: </label>
+            <input for="email" name="email" required="" type="email" />
+            <label for="password">Password: </label>
+            <input id="password" name="password" required="" type="password" />
+            <input name="register" type="submit" value="Register" />
+        </form>
+
+    </body>
+</html>
